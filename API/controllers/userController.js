7b40f6c920168804bbd3af8db5a861d31620494f
Eeshan8503 <eeshanmattey@gmail.com>
2022-06-11 01:50:23 +0530

    handled errors

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -8,21 +8,21 @@ const {isValid}=require("./developerController");
     name:"gajodahssaar",
     email:"fds@12"
   }
-const init1=catchAsync(async()=>{
- const web3=new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
-  const networkId = await web3.eth.net.getId();
-  const myContract = new web3.eth.Contract(
-        contract.abi,
-        contract.networks[networkId].address
-      );
-    web3.eth.accounts.wallet.add(privateKey);
-    const gasPrice = await web3.eth.getGasPrice();
-    const gasEstimate = await myContract.methods.addUser(user).estimateGas({ from: address });
+// const init1=catchAsync(async()=>{
+//  const web3=new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+//   const networkId = await web3.eth.net.getId();
+//   const myContract = new web3.eth.Contract(
+//         contract.abi,
+//         contract.networks[networkId].address
+//       );
+//     web3.eth.accounts.wallet.add(privateKey);
+//     const gasPrice = await web3.eth.getGasPrice();
+//     const gasEstimate = await myContract.methods.addUser(user).estimateGas({ from: address });
 
-    const temp=await myContract.methods.addUser(user).send({from:address,gasPrice: gasPrice, gas: gasEstimate});
-    const u=await myContract.methods.getUser(address).call();
-    console.log(u)
-})
+//     // const temp=await myContract.methods.addUser(user).send({from:address,gasPrice: gasPrice, gas: gasEstimate});
+//     // const u=await myContract.methods.getUser(address).call();
+//     // console.log(u)
+// })
 
 exports.getUser=catchAsync(async(req,res)=>{
   console.log(req.params.user)
@@ -38,12 +38,17 @@ const myContract = new web3.eth.Contract(
   const gasEstimate = await myContract.methods.getUser(req.params.user).estimateGas({ from: address });
 //////////////////////////////////////////////////////////////////////////
   // if(isValid){
-    const u=await myContract.methods.getUser(req.params.user).call({from:address,gasPrice: gasPrice, gas: gasEstimate});
+  try{
+    const u=await myContract.methods.getUser(req.params.user).call({from:address,gasPrice: gasPrice, gas: gasEstimate});  
     if(u)
     res.status(201).json(u);
     else{
       res.status(500).json({message:"No user exists"})
     }
+  }
+  catch(err){
+    res.status(500).json({message:err.message})
+  }
   // }
   // else{
   //   res.status(500).json({message:"Invalid Auth key"})
@@ -62,18 +67,25 @@ const web3=new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
         contract.networks[networkId].address
       );
     web3.eth.accounts.wallet.add(privateKey);
-    const gasPrice = await web3.eth.getGasPrice();
-    const gasEstimate = await myContract.methods.addUser(user).estimateGas({ from: address });
+    
 //////////////////////////////////////////////////////////////////////////
 
 
   console.log(req.body)
+  try{
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasEstimate = await myContract.methods.addUser(user).estimateGas({ from: address });
+    const temp=await myContract.methods.addUser(user).send({from:address,gasPrice: gasPrice, gas: gasEstimate});
+    if(temp){
+      res.status(201).json(temp);
+    }
+    else{
+      res.status(500).json({message:"Error adding user"});
+  }
+}
+  catch(err){
+    // console.log("hehrere")
+    res.status(500).json({message:"user already exists"})
+  }
   
-  const temp=await myContract.methods.addUser(user).send({from:address,gasPrice: gasPrice, gas: gasEstimate});
-  if(temp){
-    res.status(201).json(temp);
-  }
-  else{
-    res.status(500).json({message:"Error adding user"});
-  }
 })
