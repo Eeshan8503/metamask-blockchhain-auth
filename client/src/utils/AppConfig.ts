ee6bf878a339c87bdd16b9e15d4a7d72c727b1ad
Eeshan8503 <eeshanmattey@gmail.com>
2022-06-12 03:45:57 +0530

    deleted client

--- client/src/utils/AppConfig.ts
+++
@@ -1,7 +0,0 @@
-export const AppConfig = {
-  site_name: 'Metamask-Auth',
-  title: 'Metamask-Auth',
-  description:
-    'Using our blockchain-auth app, we connect the worlds of centralization and decentralisation. ',
-  locale: 'en',
-};
